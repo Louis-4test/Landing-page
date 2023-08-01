02a9c30e9614e56006fd789afa1a877dcf51c26a
Fola Louis <fola.louis@gmail.com>
2023-08-01 09:26:44 +0100

    javascript added and mobile responsive modified

---
+++ js/app.js
@@ -0,0 +1,8 @@
+function changeText(){
+    var h1 = document.getElementById("h1");
+    if(h1.innerHTML == "UI/UX Designer & Web Developer"){
+        h1.innerHTML = "Skilled in Design and Web Development";
+    }else{
+        h1.innerHTML = "UI/UX Designer & Web Developer";
+    }
+}
