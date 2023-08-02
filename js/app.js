03a45f43b0375ed347d24e3aaa5a3de999082961
Fola Louis <fola.louis@gmail.com>
2023-08-02 02:12:20 +0100

    modifying javascript

--- js/app.js
+++ js/app.js
@@ -1,8 +1,21 @@
 function changeText(){
-    var h1 = document.getElementById("h1");
-    if(h1.innerHTML == "UI/UX Designer & Web Developer"){
-        h1.innerHTML = "Skilled in Design and Web Development";
+    var h1 = document.getElementById("h1")
+    if(h1.innerHTML == "Ui/Ux Designer And Web Developer"){
+        h1.innerHTML = "Skilled in Design and Web Developement"
     }else{
-        h1.innerHTML = "UI/UX Designer & Web Developer";
+        h1.innerHTML = "Ui/Ux Designer And Web Developer"
     }
 }
+
+
+
+let changeImg = ()=>{
+    var img = document.getElementById("img")
+
+    if(img.src.match("./image/20191215_091110.jpg")){
+        img.src = "./image/figo.jpg"
+    }else{
+        img.src = "./image/20191215_091110.jpg"
+    }
+}
+
